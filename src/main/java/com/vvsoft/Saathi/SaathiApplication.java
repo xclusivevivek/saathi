12a10132f1d36ec758c367a4f35b4a526f7acb74
xclusivevivek <xclusivevivek@gmail.com>
2023-01-26 12:05:38 +0530

    Inital Spring Boot Setup

---
+++ src/main/java/com/vvsoft/Saathi/SaathiApplication.java
@@ -0,0 +1,13 @@
+package com.vvsoft.Saathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaathiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vvsoft/Saathi/SaathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvsoft.Saathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
