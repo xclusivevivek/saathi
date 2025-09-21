6296bc4f75c5279d185650a5556a2b637d477978
xclusivevivek <xclusivevivek@gmail.com>
2025-09-21 14:52:00 +0530

    Added Dropbox File System

--- dropbox-integration/src/main/java/com/vvsoft/saathi/drobox/filesystem/DropboxFileSystem.java
+++ dropbox-integration/src/main/java/com/vvsoft/saathi/drobox/filesystem/DropboxFileSystem.java
@@ -1,14 +1,18 @@
 package com.vvsoft.saathi.drobox.filesystem;
 
+import com.dropbox.core.DbxDownloader;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.v2.common.PathRoot;
-import com.dropbox.core.v2.files.Metadata;
+import com.dropbox.core.v2.files.*;
 import com.vvsoft.saathi.entity.persistance.StorageFileSystem;
 
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class DropboxFileSystem implements StorageFileSystem {
@@ -31,22 +35,63 @@ public class DropboxFileSystem implements StorageFileSystem {
 
     @Override
     public Stream<Path> walk(Path folder) throws IOException {
-        return Stream.empty();
+        List<Path> files = new ArrayList<>();
+        try {
+            ListFolderResult listFolderResult = client.files().listFolder(folder.toString());
+            List<Metadata> entries = listFolderResult.getEntries();
+            if(!entries.isEmpty()) {
+                Path tempDirectory = Files.createTempDirectory("saathiTemp");
+                entries.forEach( m -> {
+                    Path sourcePath = Path.of(m.getPathDisplay());
+                    Path destPath = tempDirectory.resolve(m.getName());
+                    byte[] content = readFile(sourcePath);
+                    try {
+                        Files.write(destPath,content);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    files.add(destPath);
+                });
+            }
+        } catch (DbxException e) {
+            throw new IOException("Error while walking the path " + folder,e);
+        }
+        return files.stream();
     }
 
     @Override
     public void createFile(Path createdFile, byte[] content) throws IOException {
-
+        UploadBuilder uploadBuilder = client.files().uploadBuilder(createdFile.toString());
+        uploadBuilder.withMode(WriteMode.ADD);
+        try {
+            FileMetadata fileMetadata = uploadBuilder.uploadAndFinish(new ByteArrayInputStream(content));
+        } catch (DbxException e) {
+            throw new IOException("Unable to create file " + createdFile,e);
+        }
     }
+
 
     @Override
     public void overwriteFile(Path filePath, byte[] content) throws IOException {
-
+        try {
+            Metadata fileMetadata = client.files().getMetadata(filePath.toString());
+            if(!fileMetadata.getName().equals(filePath.getFileName().toString()))
+                throw new IOException("File does not exists : " + filePath.toString());
+            UploadBuilder uploadBuilder = client.files().uploadBuilder(filePath.toString());
+            uploadBuilder.withMode(WriteMode.OVERWRITE);
+            uploadBuilder.uploadAndFinish(new ByteArrayInputStream(content));
+        } catch (DbxException e) {
+            throw new IOException("Unable to create file " + filePath,e);
+        }
     }
 
     @Override
     public void deleteFile(Path deleteFilePath) throws IOException {
-
+        try {
+            client.files().deleteV2(deleteFilePath.toString());
+        } catch (DbxException e) {
+            throw new IOException("Error in deleting file " + deleteFilePath,e);
+        }
     }
 
     @Override
@@ -59,5 +104,36 @@ public class DropboxFileSystem implements StorageFileSystem {
         }
 
         return metadata.getName().equals(folderPath.getFileName().toString());
+    }
+
+    public byte[] readFile(Path filePath) {
+        try {
+            DbxDownloader<FileMetadata> downloader = client.files().download(filePath.toString());
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            FileMetadata result = downloader.download(outStream);
+            return outStream.toByteArray();
+        } catch (DbxException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean doesFileExists(Path filePath) {
+        Metadata fileMetadata = null;
+        try {
+            fileMetadata = client.files().getMetadata(filePath.toString());
+            return fileMetadata.getName().equals(filePath.getFileName().toString());
+        } catch (DbxException e) {
+            return false;
+        }
+    }
+
+    public void deleteFolders(Path folder) throws IOException {
+        try {
+            client.files().deleteV2(folder.toString());
+        } catch (DbxException e) {
+            throw new IOException("Could not delete folder " + folder,e);
+        }
     }
 }
--- dropbox-integration/src/test/java/com/vvsoft/saathi/drobox/filesystem/DropboxFileSystemTest.java
+++ dropbox-integration/src/test/java/com/vvsoft/saathi/drobox/filesystem/DropboxFileSystemTest.java
@@ -3,21 +3,114 @@ package com.vvsoft.saathi.drobox.filesystem;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DropboxFileSystemTest {
     private final DbxClientV2 client = new DbxClientV2(DbxRequestConfig.newBuilder("dropbox/saathi-test").build(),
-            "");
+            System.getenv("DROPBOX_ACCESS_TOKEN"));
     private final DropboxFileSystem dropboxFileSystem = new DropboxFileSystem(client);
+
+    @BeforeEach
+    public void setupDropbox() throws IOException {
+        try {
+            dropboxFileSystem.deleteFolders(Path.of("/test"));
+        } catch (IOException ignore) {
+        }
+        dropboxFileSystem.createFolders(Path.of("/test/test1"));
+    }
 
     @Test
     public void doesFolderExists_returns_true_if_folder_exists_relative_to_app_sandbox_path() throws IOException {
         boolean doesFolderExists = dropboxFileSystem.doesFolderExists(Path.of("/test/test1"));
         Assertions.assertTrue(doesFolderExists);
     }
+
+    @Test
+    public void doesFolderExists_returns_false_if_folder_does_not_exists() throws IOException {
+        boolean doesFolderExists = dropboxFileSystem.doesFolderExists(Path.of("/test/test2"));
+        Assertions.assertFalse(doesFolderExists);
+    }
+
+    @Test
+    public void createFolders_creates_folder_if_folder_does_not_exists() throws IOException {
+        dropboxFileSystem.createFolders(Path.of("/test/test1/testFolderCreation"));
+        Assertions.assertTrue(dropboxFileSystem.doesFolderExists(Path.of("/test/test1/testFolderCreation")));
+    }
+
+    @Test
+    public void createFolders_creates_multiple_folders_if_folder_does_not_exists() throws IOException {
+        dropboxFileSystem.createFolders(Path.of("/test/test1/testFolderCreation/folder1/folder2"));
+        Assertions.assertTrue(dropboxFileSystem.doesFolderExists(Path.of("/test/test1/testFolderCreation/folder1/folder2")));
+    }
+
+    @Test
+    public void createFolders_raisesException_if_folder_already_exists() throws IOException {
+        dropboxFileSystem.createFolders(Path.of("/test/test1/testFolderCreation/folder1/folder2"));
+        Assertions.assertThrows(Exception.class,() ->dropboxFileSystem.createFolders(Path.of("/test/test1/testFolderCreation/folder1/folder2")));
+    }
+
+    @Test
+    public void createFile_creates_file_with_given_name_and_location_with_given_content() throws IOException {
+        dropboxFileSystem.createFile(Path.of("/test/testFile.txt"),"This is a test file".getBytes(StandardCharsets.UTF_8));
+        Assertions.assertEquals("This is a test file",new String(dropboxFileSystem.readFile(Path.of("/test/testFile.txt"))));
+    }
+
+    @Test
+    public void createFile_throws_error_if_file_already_exists() throws IOException {
+        dropboxFileSystem.createFile(Path.of("/test/testDuplicateFile.txt"),"This is a test file".getBytes(StandardCharsets.UTF_8));
+        Assertions.assertThrows(IOException.class,() -> dropboxFileSystem.createFile(Path.of("/test/testDuplicateFile.txt"),"This is a overwritten file".getBytes(StandardCharsets.UTF_8)));
+    }
+
+    @Test
+    public void overwriteFile_overwrite_file_with_given_name_and_location_with_given_content() throws IOException {
+        String filePath = "/test/overWriteFile.txt";
+        dropboxFileSystem.createFile(Path.of(filePath),"This is a test file".getBytes(StandardCharsets.UTF_8));
+        dropboxFileSystem.overwriteFile(Path.of(filePath),"This is a overwritten file".getBytes(StandardCharsets.UTF_8));
+        Assertions.assertEquals("This is a overwritten file",new String(dropboxFileSystem.readFile(Path.of(filePath))));
+    }
+
+    @Test
+    public void overwriteFile_throws_exception_if_file_doesnot_exists() throws IOException {
+        String filePath = "/test/overWriteFile123.txt";
+        Assertions.assertThrows(IOException.class,() -> dropboxFileSystem.overwriteFile(Path.of(filePath),"This is a overwritten file".getBytes(StandardCharsets.UTF_8)));
+    }
+
+    @Test
+    public void deleteFile_deletes_file_with_given_path() throws IOException {
+        String filePath = "/test/fileToDelete.txt";
+        dropboxFileSystem.createFile(Path.of(filePath),"This is a test file".getBytes(StandardCharsets.UTF_8));
+        dropboxFileSystem.deleteFile(Path.of(filePath));
+        Assertions.assertFalse(dropboxFileSystem.doesFileExists(Path.of(filePath)));
+    }
+
+    @Test
+    public void deleteFile_throws_exception_if_file_does_not_exists() throws IOException {
+        String filePath = "/test/fileToDeleteRandom.txt";
+        Assertions.assertThrows(IOException.class,() -> dropboxFileSystem.deleteFile(Path.of(filePath)));
+    }
+
+    @Test
+    public void walk_returns_all_objects_at_folder_location() throws IOException {
+        Path walkPath = Path.of("/test/walk/");
+        dropboxFileSystem.createFolders(walkPath);
+        dropboxFileSystem.createFile(walkPath.resolve("file1.txt"),"file1.txt".getBytes(StandardCharsets.UTF_8));
+        dropboxFileSystem.createFile(walkPath.resolve("file2.txt"),"file2.txt".getBytes(StandardCharsets.UTF_8));
+        Stream<Path> paths = dropboxFileSystem.walk(walkPath);
+        List<Path> pathsList = paths.collect(Collectors.toList());
+        Assertions.assertEquals(2,pathsList.size());
+        Assertions.assertTrue(pathsList.stream().anyMatch(p -> p.getFileName().toString().equals("file1.txt")));
+        Assertions.assertTrue(pathsList.stream().anyMatch(p -> p.getFileName().toString().equals("file2.txt")));
+    }
+
+
 }
